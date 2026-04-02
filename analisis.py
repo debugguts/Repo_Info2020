9f070721279f40b57e5ee395fc8df7697e68a015
Gustavo <gutss11ao@gmail.com>
2026-04-02 03:25:39 -0300

    Agrega funcion limpiar_datos en develop

--- analisis.py
+++ analisis.py
@@ -6,3 +6,14 @@ print("--- Analisis de datos ---")
 print("Promedio:", statistics.mean(datos))
 print("Mediana:", statistics.median(datos))
 print("Desv. estandar:", round(statistics.stdev(datos), 2))
+
+def limpiar_datos(lista):
+    """Elimina valores negativos y nulos de la lista."""
+    return [x for x in lista if x is not None and x > 0]
+
+datos_sucios = [10, -5, None, 30, 0, 50, None, 20]
+datos_limpios = limpiar_datos(datos_sucios)
+
+print("\n--- Limpieza de datos ---")
+print("Datos originales:", datos_sucios)
+print("Datos limpios:   ", datos_limpios)
